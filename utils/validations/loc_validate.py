e54e1dec970308c371047ac174265fef7403b799
Boniface Murangiri <bonnyrangi95@gmail.com>
2025-01-22 16:17:04 +0300

    feat: Add Marshmallow schema for Location validation with unique name check.

---
+++ utils/validations/loc_validate.py
@@ -0,0 +1,14 @@
+from marshmallow import fields, validate, validates, ValidationError
+from app.extensions import ma
+from app.models.v1 import Location
+
+
+class RegLocSchema(ma.Schema):
+    name = fields.Str(required=True, validate=validate.Length(min=1, max=120))
+    address = fields.Str(
+        required=True, validate=validate.Length(min=1, max=120))
+
+    @validates("name")
+    def validate_name(self, value):
+        if Location.query.filter(Location.name.ilike(value)).first():
+            raise ValidationError(f"Location name '{value}' already exists.")
