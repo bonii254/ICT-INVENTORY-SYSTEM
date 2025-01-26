3286a805f0f2b4c48dd6e7d3ca4b7b3de9de21e7
Boniface Murangiri <bonnyrangi95@gmail.com>
2025-01-26 18:35:25 +0300

    Add validation schema for udating ticket.

--- utils/validations/ticket_validate.py
+++ utils/validations/ticket_validate.py
@@ -52,3 +52,45 @@ class RegTicSchema(ma.Schema):
         """
         if not User.query.get(value):
             raise ValidationError(f"User with id {value} does not exist.")
+
+
+class UpdateTicSchema(ma.Schema):
+    asset_id = fields.Int()
+    user_id = fields.Int()
+    status = fields.String(
+        validate=validate.OneOf(
+            ["Open", "In Progress", "Closed"],
+            error=(
+                "The 'status' must be one of: 'Open', "
+                "'In Progress', or 'Closed'."
+            )
+        ),
+    )
+    description = fields.Str(
+        required=False,
+        allow_none=True,
+        validate=validate.Length(max=1000)
+    )
+    resolution_notes = fields.Str(
+        required=False,
+        allow_none=True,
+        validate=validate.Length(max=1000)
+    )
+
+    @validates("asset_id")
+    def validate_asset_id(self, value):
+        """
+        Ensure the asset_id exists in the database.
+        """
+        if value:
+            if not Asset.query.get(value):
+                raise ValidationError(f"Asset with id {value} does not exist.")
+
+    @validates("user_id")
+    def validate_user_id(self, value):
+        """
+        Ensure the user_id exists in the database.
+        """
+        if value:
+            if not User.query.get(value):
+                raise ValidationError(f"User with id {value} does not exist.")
