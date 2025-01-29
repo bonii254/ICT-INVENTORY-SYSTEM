a204464524876ada3e1bc8d6698c11d224d6caba
Boniface Murangiri <bonnyrangi95@gmail.com>
2025-01-29 18:47:36 +0300

    Add validation for updating software.

--- utils/validations/software_validation.py
+++ utils/validations/software_validation.py
@@ -25,3 +25,30 @@ class RegSoftwareSchema(ma.Schema):
     def validate_expirydate(self, value):
         if value and value < datetime.utcnow().date():
             raise ValidationError("The 'expiry_date' cannot be in the past.")
+
+
+class UpdateSoftwareSchema(ma.Schema):
+    name = fields.Str(validate=validate.Length(min=1, max=120))
+    version = fields.Str(allow_none=True, validate=validate.Length(max=120))
+    license_key = fields.Str(
+        allow_none=True, validate=validate.Length(max=300))
+    expiry_date = fields.Date(
+        allow_none=True,
+        error_messages={
+            "invalid": "Invalid date format. Use 'YYYY-MM-DD'."
+        }
+    )
+
+    @validates("name")
+    def validate_name(self, value):
+        if value:
+            if Software.query.filter(Software.name.ilike(value)).first():
+                raise ValidationError(
+                    f"Software name '{value}' already exists.")
+
+    @validates("expiry_date")
+    def validate_expirydate(self, value):
+        if value:
+            if value and value < datetime.utcnow().date():
+                raise ValidationError(
+                    "The 'expiry_date' cannot be in the past.")
