be251beb1d904b3e534f8007afbae097fc5b9cc9
Boniface Murangiri <bonnyrangi95@gmail.com>
2025-02-28 20:25:36 +0300

    Remove provisioning of asset payload in each test adding it to fixture.

--- tests/testApi/testAsset/test_createasset.py
+++ tests/testApi/testAsset/test_createasset.py
@@ -1,12 +1,12 @@
 def test_create_asset(asset_user_client):
     """Test successful asset creation."""
-    client, headers, asset_info = asset_user_client
+    client, headers, asset_info, _ = asset_user_client
     response = client.post("/register/asset", json=asset_info, headers=headers)
     assert response.status_code == 201
 
 
 def test_create_asset_missing_fields(asset_user_client):
-    client, headers, _ = asset_user_client
+    client, headers, _, _ = asset_user_client
     response = client.post(
         "/register/asset",
         json={"category_id": 1, "location_id": 1}, headers=headers)
@@ -17,7 +17,7 @@ def test_create_asset_missing_fields(asset_user_client):
 
 def test_create_asset_invalid_json(asset_user_client):
     """Test invalid JSON format"""
-    client, headers, _ = asset_user_client
+    client, headers, _, _ = asset_user_client
     response = client.post("/register/asset",
                            data="{invalid_json}",
                            headers=headers)
@@ -28,7 +28,7 @@ def test_create_asset_invalid_json(asset_user_client):
 
 def test_create_asset_duplicate_entry(asset_user_client):
     """Test duplicate asset creation"""
-    client, headers, asset_info = asset_user_client
+    client, headers, asset_info, _ = asset_user_client
     client.post("/register/asset",
                 json=asset_info,
                 headers=headers)
@@ -39,7 +39,7 @@ def test_create_asset_duplicate_entry(asset_user_client):
 
 
 def test_create_asset_unexpected_error(asset_user_client, mocker):
-    client, headers, asset_info = asset_user_client
+    client, headers, asset_info, _ = asset_user_client
     mocker.patch("app.db.session.commit", side_effect=Exception("DB Error"))
     response = client.post("/register/asset", json=asset_info, headers=headers)
     assert response.status_code == 500
@@ -48,7 +48,7 @@ def test_create_asset_unexpected_error(asset_user_client, mocker):
 
 def test_create_asset_invalid_mac_address_format(asset_user_client):
     """Test invalid MAC address format"""
-    client, headers, asset_info = asset_user_client
+    client, headers, asset_info, _ = asset_user_client
     asset_info["mac_address"] = "ZZ:ZZ:ZZ:ZZ:ZZ:ZZ"
     response = client.post("/register/asset",
                            json=asset_info,
@@ -60,7 +60,7 @@ def test_create_asset_invalid_mac_address_format(asset_user_client):
 
 def test_create_asset_no_mac_address_format(asset_user_client):
     """Test None MAC address"""
-    client, headers, asset_info = asset_user_client
+    client, headers, asset_info, _ = asset_user_client
     asset_info["mac_address"] = None
     response = client.post("/register/asset",
                            json=asset_info,
@@ -70,7 +70,7 @@ def test_create_asset_no_mac_address_format(asset_user_client):
 
 def test_create_asset_invalid_assigned_to(asset_user_client):
     """Test assigning asset to a non-existent user"""
-    client, headers, asset_info = asset_user_client
+    client, headers, asset_info, _ = asset_user_client
     asset_info["assigned_to"] = 9999
     response = client.post("/register/asset",
                            json=asset_info,
@@ -81,7 +81,7 @@ def test_create_asset_invalid_assigned_to(asset_user_client):
 
 def test_create_asset_invalid_category(asset_user_client):
     """Test invalid category_id"""
-    client, headers, asset_info = asset_user_client
+    client, headers, asset_info, _ = asset_user_client
     asset_info["category_id"] = 9999
     response = client.post("/register/asset",
                            json=asset_info,
